281f1ac086436c727f763f8a87bae2c6d84642a2
woobinpro <woobinpro@gmail.com>
2023-03-17 06:33:15 +0000

    Setup client site

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import Home from './Pages/Home/Home/Home';
+
+function App() {
+  return (
+    <div className="">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
